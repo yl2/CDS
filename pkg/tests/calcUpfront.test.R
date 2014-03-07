ed37f9811ea110cc323b2554c6edf0c54e54bec8
Yang Lu <yang.lu2014@gmail.com>
2014-03-07 12:53:24 -0500

    add rdata

---
+++ pkg/tests/calcUpfront.test.R
@@ -0,0 +1,50 @@
+## calcUpfront.test.R
+
+library(CDS)
+
+## truth <- calcUpfront("2008-01-03",
+##                      types = "MMMMMSSSSSSSSS",
+##                      dates = c(20080201, 20080203, 20080205),
+##                      rates = c(1e-9, 1e-9, 1e-9, 1e-9, 1e-9, 1e-9, 1e-9, 1e-9, 1e-9, 1e-9, 1e-9, 1e-9, 1e-9, 1e-9),
+##                      nInstr = 10,
+##                      mmDCC = 10,
+##                      fixedSwapFreq = 10,
+##                      floatSwapFreq = 10,
+##                      fixedSwapDcc = 10,
+##                      floatSwapDcc = 10,
+##                      badDayConvZC = 'M',
+##                      holidays = 'None',
+##                      today = "2008-02-01",
+##                      valueDate = "2008-02-01",
+##                      benchmarkDate = "2008-02-02",
+##                      startDate = "2008-02-08",
+##                      endDate = "2008-02-12",
+##                      stepinDate = "2008-02-9",
+##                      parSpread = 720,
+##                      couponRate = 100)
+## save(truth, file = "calcUpfront.test.RData")
+
+load("calcUpfront.test.RData")
+result <- calcUpfront("2008-01-03",
+                      types = "MMMMMSSSSSSSSS",
+                      dates = c(20080201, 20080203, 20080205),
+                      rates = c(1e-9, 1e-9, 1e-9, 1e-9, 1e-9, 1e-9, 1e-9, 1e-9, 1e-9, 1e-9, 1e-9, 1e-9, 1e-9, 1e-9),
+                      nInstr = 10,
+                      mmDCC = 10,
+                      fixedSwapFreq = 10,
+                      floatSwapFreq = 10,
+                      fixedSwapDcc = 10,
+                      floatSwapDcc = 10,
+                      badDayConvZC = 'M',
+                      holidays = 'None',
+                      today = "2008-02-01",
+                      valueDate = "2008-02-01",
+                      benchmarkDate = "2008-02-02",
+                      startDate = "2008-02-08",
+                      endDate = "2008-02-12",
+                      stepinDate = "2008-02-9",
+                      parSpread = 720,
+                      couponRate = 100)
+
+
+stopifnot(all.equal(result, truth))
