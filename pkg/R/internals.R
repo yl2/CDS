195d5b710096345102612b6bb4b094f988a36453
Yang Lu <yang.lu2014@gmail.com>
2014-04-06 18:18:56 -0400

    update

--- pkg/R/internals.R
+++ pkg/R/internals.R
@@ -1,4 +1,4 @@
-separateYMD <- function(d){
+.separateYMD <- function(d){
     ## valueDate format valueDate = "2008-02-01"
     dateYear <- as.numeric(format(as.Date(d), "%Y"))
     dateMonth <- as.numeric(format(as.Date(d), "%m"))
