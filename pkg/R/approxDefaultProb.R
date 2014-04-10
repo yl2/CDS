e9cea83e7c49054870c286e5d5c3120b8e859ef0
Heidi Chen <s.heidi.chen@gmail.com>
2014-04-10 16:01:24 -0400

    default prob

--- pkg/R/approxDefaultProb.R
+++ pkg/R/approxDefaultProb.R
@@ -2,10 +2,11 @@
 #'
 #' @param parSpread in bps
 #' @param t in years
+#' @param recoveryRate in decimal. Default is 0.4.
 #' 
-approxDefaultProb <- function(parSpread, t, recovery.rate = 0.4){
+approxDefaultProb <- function(parSpread, t, recoveryRate = 0.4){
     ## Bloomberg Approximation
-    prob = 1 - exp(-parSpread/1e4*t/(1 - recovery.rate))
+    prob = 1 - exp(-parSpread/1e4*t/(1 - recoveryRate))
 
     return(prob)
 
--- pkg/tests/approxDefaultProb.test.R
+++ pkg/tests/approxDefaultProb.test.R
@@ -2,12 +2,12 @@
 
 library(CDS)
 
-## truth <- approxDefaultProb(parSpread = 32, t = 5.25)
+## truth <- approxDefaultProb(parSpread = 32, t = 5.25, recoveryRate = 0.4)
 
 ## save(truth, file = "approxDefaultProb.test.RData")
 
 load("approxDefaultProb.test.RData")
 
-result <- approxDefaultProb(parSpread = 32, t = 5.25)
+result <- approxDefaultProb(parSpread = 32, t = 5.25, recoveryRate = 0.4)
 
 stopifnot(all.equal(result, truth))
