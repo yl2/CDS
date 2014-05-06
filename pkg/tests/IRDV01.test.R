f9778909ea840e69a76a783c1bc9986e52e94f36
Yang Lu <yang.lu2014@gmail.com>
2014-05-05 21:41:18 -0400

    test cases

---
+++ pkg/tests/IRDV01.test.R
@@ -0,0 +1,37 @@
+## calcDV01.test.R
+library(CDS)
+
+## truth1 <- IRDV01(TDate = "2014-01-14",
+##                  currency = "USD",
+##                  maturity = "5Y",
+##                  dccCDS = "Act/360",
+##                  freqCDS = "1Q",
+##                  stubCDS = "F",
+##                  badDayConvCDS = "F",
+##                  calendar = "None",
+##                  parSpread = 32,
+##                  couponRate = 100,
+##                  recoveryRate = 0.4,
+##                  notional = 1e7)
+
+## save(truth1, file = "IRDV01.test.RData")
+
+
+
+load("IRDV01.test.RData")
+
+
+result1 <- IRDV01(TDate = "2014-01-14",
+                  currency = "USD",
+                  maturity = "5Y",
+                  dccCDS = "Act/360",
+                  freqCDS = "1Q",
+                  stubCDS = "F",
+                  badDayConvCDS = "F",
+                  calendar = "None",
+                  parSpread = 32,
+                  couponRate = 100,
+                  recoveryRate = 0.4,
+                  notional = 1e7)
+
+stopifnot(all.equal(result1, truth1))
---
+++ pkg/tests/recRisk01.test.R
@@ -0,0 +1,37 @@
+## calcRecRisk01.test.R
+
+library(CDS)
+
+## truth1 <- recRisk01(TDate = "2014-01-14",
+##                     currency = "USD",
+##                     maturity = "5Y",
+##                     dccCDS = "Act/360",
+##                     freqCDS = "1Q",
+##                     stubCDS = "F",
+##                     badDayConvCDS = "F",
+##                     calendar = "None",
+##                     parSpread = 32,
+##                     couponRate = 100,
+##                     recoveryRate = 0.4,
+##                     notional = 1e7)
+
+## save(truth1, file = "recRisk01.test.RData")
+
+
+load("recRisk01.test.RData")
+
+result1 <- recRisk01(TDate = "2014-01-14",
+                     currency = "USD",
+                     maturity = "5Y",
+                     dccCDS = "Act/360",
+                     freqCDS = "1Q",
+                     stubCDS = "F",
+                     badDayConvCDS = "F",
+                     calendar = "None",
+                     parSpread = 32,
+                     couponRate = 100,
+                     recoveryRate = 0.4,
+                     notional = 1e7)
+
+stopifnot(all.equal(result1, truth1))
+
